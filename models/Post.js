0e43766becb5bf18ecda2e4c4d6682b08ef765ae
sooman992 <fm01793176380@gmail.com>
2025-07-05 20:22:18 +0600

    Image upload bug fixed

--- models/Post.js
+++ models/Post.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const PostSchema = new mongoose.Schema({
     title: { type: String, required: true },
     content: { type: String, required: true },
+    imageUrl: {type: String},
     author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     createdAt: { type: Date, default: Date.now }
 });
